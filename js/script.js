4d3533d429f904c0ee11f2c6dba09c273ee7a257
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-04-23 15:10:18 +0200

    correzioni

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 // 1. Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
 // 2. Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 //    Click sul contatto mostra la conversazione del contatto cliccato
+// 3. Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+//    Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 
 var app = new Vue (
     {
@@ -95,6 +97,7 @@ var app = new Vue (
             
         },
         methods: {
+            // Funzione che mostra la conversazione del contatto cliccato
             showChat(index) {
                 this.activeContact = index;
             }
